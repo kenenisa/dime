2d8fb2b6138e085f7b10018f66c78f0b6cffbac6
Kenenisa Alemayehu <kenenisaalemayhu0@gmail.com>
2023-04-19 23:09:25 +0300

    Added: ML get training data

--- app/Controllers/PredictionsController.ts
+++ app/Controllers/PredictionsController.ts
@@ -1,8 +1,77 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Account from 'App/Models/Account'
+const average = array => array.reduce((a, b) => a + b) / array.length;
+const max = array => array.reduce((a, b) => { return Math.max(a, b) });
+const total = array => array.reduce((a, b) => { return a + b },0);
 
 export default class PredictionsController {
     public async getPredictionTraining({ }: HttpContextContract) {
-        return ["hey"]
+        const accounts = await Account.all()
+        const result: any[] = []
+        for (let account of accounts) {
+            const transactions = await Database
+                .from("transactions")
+                .where("account_id", account.id)
+                .exec()
+            const df = {}
+            transactions.forEach(tx => {
+                const date = new Date(tx.created_at).toLocaleDateString()
+                if (!df[date]) {
+                    df[date] = { deposit: [], expense: [] }
+                }
+                if (tx.deposit) {
+                    df[date].deposit.push(tx.amount)
+                } else {
+                    df[date].expense.push(tx.amount)
+                }
+            })
+            const days: any = []
+            for (let a in df) {
+                days.push({
+                    date: a,
+                    deposit: {
+                        total: total(df[a].deposit),
+                        max: max(df[a].deposit),
+                        average: average(df[a].deposit),
+                    },
+                    expense: {
+                        total: total(df[a].expense),
+                        max: max(df[a].expense),
+                        average: average(df[a].expense),
+                    }
+                })
+            }
+            result.push({
+                address: account.address,
+                days
+            })
+        }
+        return result
+    }
+
+    public async getReserveTraining({ }: HttpContextContract) {
+        const accounts = await Account.all()
+        const result: any[] = []
+        for (let account of accounts) {
+            const transactions = await Database
+                .from("transactions")
+                .where("account_id", account.id)
+                .exec()
+
+            const total_expense = total(transactions.filter(v => !v.deposit).map(item => item.amount));
+            const total_deposit = total(transactions.filter(v => v.deposit).map(item => item.amount));
+            const active = true;
+            const credit_score = account.creditScore
+            result.push({
+                total_deposit,
+                total_expense,
+                active,
+                credit_score,
+                address: account.address
+            })
+        }
+        return result
     }
 
 }
