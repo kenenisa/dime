004bc705ce7d3fb48aaa5a2ec71b73891ab099a2
Kenenisa Alemayehu <kenenisaalemayhu0@gmail.com>
2023-04-19 20:49:07 +0300

    Fix: build

--- app/Controllers/PredictionsController.ts
+++ app/Controllers/PredictionsController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PredictionsController {
-
+    public async getPredictionTraining({ }: HttpContextContract) {
+        return ["hey"]
+    }
 
 }
