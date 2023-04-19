fc15cbbd75d800e283d2df90b9cbf65a7dd5d522
Kenenisa Alemayehu <kenenisaalemayhu0@gmail.com>
2023-04-20 00:18:09 +0300

    Fixed: remove faker factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,12 +1,1 @@
-import Factory from '@ioc:Adonis/Lucid/Factory'
-import Account from 'App/Models/Account'
-
-
-
-export const AccountFactory = Factory
-  .define(Account, ({ faker }) => {
-    return {
-     
-    }
-  })
-  .build()
+// import Factory from '@ioc:Adonis/Lucid/Factory'
